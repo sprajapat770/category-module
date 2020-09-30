1cd0bd3c393c7bf704a8c50f0a3c7c56546f221c
Suraj Prajapat <sprajapat770@gmail.com>
2020-09-30 18:02:57 +0530

    Changes in Block

--- Block/CategoryList.php
+++ Block/CategoryList.php
@@ -14,16 +14,6 @@ class CategoryList extends Template
 {
 
     /**
-     * @var CollectionFactory $collection
-     */
-    public $collection;
-
-    /**
-     * @var Collection $collectionData
-     */
-    private $collectionData;
-
-    /**
      * @var StoreManagerInterface $_storeManager
      */
     protected $_storeManager;
@@ -33,46 +23,21 @@ class CategoryList extends Template
      */
     private $categoryRepository;
 
-    /**
-     * @var CategoryFactory
-     */
-    private $categoryFactory;
-
-    /**
-     * @var Registry
-     */
-    protected $_registry;
 
     public function __construct(
         Template\Context $context,
-        Registry $registry,
-        CategoryFactory $categoryFactory,
         CategoryRepository $categoryRepository,
         StoreManagerInterface $storeManager,
-        CollectionFactory $collectionFactory,
         array $data = []
     ) {
         $this->categoryRepository = $categoryRepository;
-        $this->collection = $collectionFactory;
         $this->_storeManager = $storeManager;
-        $this->categoryFactory = $categoryFactory;
-        $this->_registry = $registry;
         parent::__construct($context, $data);
     }
 
     public function getCategoryList()
     {
-             $levels = 2;
-           /*  $categories=  $this->collection->create()->addAttributeToSelect('*')
-                 ->setStore()
-                 ->addAttributeToFilter('is_active', '1');
-             return $categories;*/
         $rootCategory=  $this->_storeManager->getStore()->getRootCategoryId();
-      //  $parentId = 2;
-
-     //   $currentCategory = $this->_registry->create()->registry('current_category');
-      // $parentId =  $currentCategory->getId();
-
         $categoryObj = $this->categoryRepository->get($rootCategory);
         return $categoryObj->getChildrenCategories();
     }
